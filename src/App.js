12d66d4408d15a3fc52e0e79a083f1a88b2f05c3
Shipu8644 <shipu.monjurur@gmail.com>
2021-09-19 04:06:37 +0600

    increDecre

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
+      <Counter></Counter>
       <h1>ClothStore</h1>
       <SearchProduct></SearchProduct>
       <ExploreProduct></ExploreProduct>
@@ -27,13 +28,14 @@ function SearchProduct() {
     padding: '5px 10px',
     marginLeft: '5px',
     borderRadius: '5px',
-    fontSize: '30px'
+    fontSize: '30px',
+    fontWeight: "bold"
   }
 
   return (
     <div>
       <input style={inputStyle} type="text" />
-      <button onClick={ExploreProduct} style={buttonStyle}>Search</button>
+      <button style={buttonStyle}>Search</button>
     </div>
   );
 }
@@ -49,21 +51,46 @@ function ExploreProduct() {
     <div className='productStyle'>
       {
         products.map(product => <ProductsElements Category={product.category}
-          Pic={product.image} Price={product.price} ></ProductsElements>)
+          Pic={product.image} Price={product.price} count={product.rating.count} rating={product.rating.rate} ></ProductsElements>)
       }
 
     </div>
   );
 }
 function ProductsElements(props) {
-  const { Category, Pic, Price } = props;
+  const { Category, Pic, Price, count, rating } = props;
+
   return (
     <div className='singleProductStyle'>
       <h2>Category: {Category}</h2>
       <img style={{ width: '200px' }} src={Pic} alt="" />
       <h3>Price: ${Price}</h3>
+      <h3>Rating Count: ${count}</h3>
+      <h3>Rating : ${rating}</h3>
 
     </div>
+  );
+}
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handledecrease = () => setCount(count - 1);
+  const handleIncrease = () => setCount(count + 1);
+  const clearValue = () => setCount(0);
+
+  if (count < 0) {
+    clearValue();
+  }
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button className='inCreDecreButtonStyle' onClick={handledecrease}>Decrease</button>
+      <button className='inCreDecreButtonStyle' onClick={handleIncrease}>Increase</button>
+      <button className='inCreDecreButtonStyle' onClick={clearValue}>Clear</button>
+
+    </div>
+
   );
 }
 
