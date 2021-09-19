ebd0d5f63875536eae3be92c26a93ab3b17496ad
Shipu8644 <shipu.monjurur@gmail.com>
2021-09-19 19:08:51 +0600

    todo added

--- src/App.js
+++ src/App.js
@@ -7,38 +7,12 @@ function App() {
     <div className="App">
       <Counter></Counter>
       <h1>ClothStore</h1>
-      <SearchProduct></SearchProduct>
       <ExploreProduct></ExploreProduct>
+      <SearchTodo></SearchTodo>
     </div>
   );
 }
-// inputField Sector
-function SearchProduct() {
-  const inputStyle = {
-    backgroundColor: 'black',
-    color: 'white',
-    fontSize: '30px',
-    fontWeight: "bold",
-    padding: "10px",
-    border: '3px solid Red',
-    borderRadius: '5px'
 
-  }
-  const buttonStyle = {
-    padding: '5px 10px',
-    marginLeft: '5px',
-    borderRadius: '5px',
-    fontSize: '30px',
-    fontWeight: "bold"
-  }
-
-  return (
-    <div>
-      <input style={inputStyle} type="text" />
-      <button style={buttonStyle}>Search</button>
-    </div>
-  );
-}
 
 function ExploreProduct() {
   const [products, setProducts] = useState([]);
@@ -91,23 +65,94 @@ function ProductsElements(props) {
 
 
 function Counter() {
-  // const [count, setCount] = useState(0);
-  // const handledecrease = () => setCount(count - 1);
-  // const handleIncrease = () => setCount(count + 1);
-  // const clearValue = () => setCount(0);
+  const [count, setCount] = useState(50);
+  const handledecrease = () => setCount(count - 1);
+  const handleIncrease = () => setCount(count + 1);
+  const clearValue = () => setCount(0);
 
-  // if (count < 0) {
-  //   clearValue();
-  // }
+  if (count < 0) {
+    clearValue();
+  }
+  if (count > 100) {
+    setCount(100);
+  }
   return (
     <div>
-      <h1 id='add-remove'>Product Count: </h1>
-      {/* <button className='inCreDecreButtonStyle' onClick={handledecrease}>Decrease</button>
+      <h1 id='add-remove'>Battery:{count}% </h1>
+      <button className='inCreDecreButtonStyle' onClick={handledecrease}>Decrease</button>
       <button className='inCreDecreButtonStyle' onClick={handleIncrease}>Increase</button>
-      <button className='inCreDecreButtonStyle' onClick={clearValue}>Clear</button> */}
+      <button className='inCreDecreButtonStyle' onClick={clearValue}>Clear</button>
 
     </div>
 
+  );
+}
+
+// inputField Sector
+function SearchTodo() {
+  const [todos, setTodo] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(data => setTodo(data))
+  }, [])
+  const inputStyle = {
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: '30px',
+    fontWeight: "bold",
+    padding: "10px",
+    border: '3px solid Red',
+    borderRadius: '5px'
+
+  }
+  const buttonStyle = {
+    padding: '5px 10px',
+    marginLeft: '5px',
+    borderRadius: '5px',
+    fontSize: '30px',
+    fontWeight: "bold"
+  }
+  const handleTodo = () => {
+
+  }
+
+  return (
+    <div>
+      <header>
+        <h1 style={{ color: 'red' }}>Welcome to todo section</h1>
+      </header>
+      <main>
+        <article className="blog">
+          <h2>You can search todo list in the below search box by numeric id</h2>
+        </article>
+        <section className='todo'>
+          <input style={inputStyle} type="text" />
+          <button onClick={handleTodo} style={buttonStyle}>Search</button>
+        </section>
+        <section >
+          <div className='productStyle'>
+
+            {todos.map(todo => <Todolist title={todo.title} id={todo.id}
+              completed={todo.completed}></Todolist>)}
+          </div>
+
+        </section>
+
+      </main>
+
+    </div>
+  );
+}
+function Todolist(props) {
+  const { title, id, completed } = props;
+  return (
+    <div className='singleProductStyle'>
+      <h2>Title:{title}</h2>
+      <h3>Id:{id}</h3>
+      <h3>Completed:{completed}</h3>
+
+    </div>
   );
 }
 
